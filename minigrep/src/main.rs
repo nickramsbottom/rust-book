acff120712a1707a514bd51034a21ebcc475e101
Nick Ramsbottom <hi@nickramsbottom.com>
2021-11-24 17:02:02 +0000

    Extract parse config from main

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -3,8 +3,7 @@ use std::{env, fs};
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let filename = &args[2];
+    let (query, filename) = parse_config(&args);
 
     println!("Searching for {}", query);
     println!("In file {}", filename);
@@ -13,4 +12,10 @@ fn main() {
         .expect("Something went wrong reading the file");
 
     println!("With text:\n{}", contents);
+}
+
+fn parse_config(args: &[String]) -> (&str, &str) {
+    let query = &args[1];
+    let filename = &args[2];
+    (query, filename)
 }
