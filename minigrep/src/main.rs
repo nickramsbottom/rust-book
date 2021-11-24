7a7992347f45397cc0aad398ae3fa75757b2e712
Nick Ramsbottom <hi@nickramsbottom.com>
2021-11-24 17:44:37 +0000

    Replace printing for tests

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -5,13 +5,9 @@ use minigrep::Config;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Provblem parsing arguments: {}", err);
+    let config = Config::new(&args).unwrap_or_else(|_err| {
         process::exit(1);
     });
-
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.filename);
 
     if let Err(e) = minigrep::run(config) {
         println!("Application error: {}", e);
