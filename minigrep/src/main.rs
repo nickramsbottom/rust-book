000e381edc83a221afc347e084f87d421ba12c90
Nick Ramsbottom <hi@nickramsbottom.com>
2021-11-24 17:24:25 +0000

    Handle run errors in main

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,4 +1,5 @@
 use std::{env, fs, process};
+use std::error::Error;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -11,14 +12,19 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
 
-    run(config);
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
+
+        process::exit(1);
+    };
 }
 
-fn run(config: Config) {
-    let contents = fs::read_to_string(config.filename)
-        .expect("Something went wrong reading the file");
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
 
     println!("With text:\n{}", contents);
+
+    Ok(())
 }
 
 struct Config {
