f7315c51a499da4b9c2321fe7c8310d2d5ee49ab
Nick Ramsbottom <hi@nickramsbottom.com>
2021-11-24 07:40:55 +0000

    Create static constructor

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -12,6 +12,13 @@ impl Rectangle {
     fn can_hold(&self, other: &Self) -> bool {
         (self.width > other.width) && (self.height > other.height)
     }
+
+    fn square(size: u32) -> Rectangle {
+        Rectangle {
+            width: size,
+            height: size,
+        }
+    }
 }
 
 fn main() {
