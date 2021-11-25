88bb47e97a44db614bcb54c80bf0f70c651154c5
Nick Ramsbottom <hi@nickramsbottom.com>
2021-11-25 12:31:28 +0000

    Activate lint on save

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -1,5 +1,5 @@
 use std::error::Error;
-use std::{ fs, env };
+use std::{env, fs};
 
 pub struct Config {
     pub query: String,
@@ -17,10 +17,10 @@ impl Config {
 
         let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
 
-        Ok(Config{
+        Ok(Config {
             query,
             filename,
-            case_sensitive
+            case_sensitive,
         })
     }
 }
@@ -53,10 +53,7 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     results
 }
 
-pub fn search_case_insensitive<'a>(
-    query: &str,
-    contents: &'a str,
-) -> Vec<&'a str> {
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let query = query.to_lowercase();
     let mut results = Vec::new();
 
